ece990ab2af2be696c85eef5d7146b71c17ebbb7
Teale Fristoe <fristoe@gmail.com>
2023-05-16 10:33:56 -0700

    Adjust base zoom. Remove default vite garbage.

--- src/map.tsx
+++ src/map.tsx
@@ -12,7 +12,7 @@ interface IMapProps {
 }
 export function Map({ latitude, locations, longitude }: IMapProps) {
   return (
-    <MapContainer center={[latitude, longitude]} zoom={13} scrollWheelZoom={false} touchZoom={true} >
+    <MapContainer center={[latitude, longitude]} zoom={9} scrollWheelZoom={false} touchZoom={true} >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
