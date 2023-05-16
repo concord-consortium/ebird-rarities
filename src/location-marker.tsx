0a326cd7e8a212807e28c5e3a45adcfbbb45829b
Teale Fristoe <fristoe@gmail.com>
2023-05-16 10:30:52 -0700

    Color code locations based on observation count.

--- src/location-marker.tsx
+++ src/location-marker.tsx
@@ -8,9 +8,15 @@ interface ILocationMarkerProps {
   observations: Observation[]
 }
 export function LocationMarker({ latitude, longitude, observations }: ILocationMarkerProps) {
-  const radius = 5 + observations.length;
+  // colorFactor should be [0-1]. 0 is rendered as blue, 1 is rendered as red, and intermediate numbers are various shades of purple.
+  const colorFactor = Math.round(Math.min((observations.length - 1) / 4, 1) * 255);
+  const rawRed = colorFactor.toString(16);
+  const red = rawRed.length < 2 ? `0${rawRed}` : rawRed;
+  const rawBlue = (255 - colorFactor).toString(16);
+  const blue = rawBlue.length < 2 ? `0${rawBlue}` : rawBlue;
+  const color = `#${red}00${blue}`;
   return (
-    <CircleMarker center={[latitude, longitude]} radius={radius} pathOptions={{ color: "red", fillColor: "blue", fillOpacity: .5 }}>
+    <CircleMarker center={[latitude, longitude]} radius={10} pathOptions={{ color, fillColor: color, fillOpacity: .4 }}>
       <Popup>
         Number of observations: {observations.length}
       </Popup>
